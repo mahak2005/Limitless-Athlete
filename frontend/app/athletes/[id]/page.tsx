40438f0dff92048267a40f5cb992c4e93dccbaf0
Mahak <mahakyadav0520@gmail.com>
2025-02-18 01:46:20 +0530

    athlete page

--- frontend/app/athletes/[id]/page.tsx
+++ frontend/app/athletes/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/layout/navbar"
 import { Footer } from "@/components/layout/footer"
 import { AthleteProfile } from "@/components/sections/athlete-profile"
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function AthletePage({ params }: { params: { id: string } }) {
+export default function AthletePage() {
     const id = "1";
     return (
         <div className="min-h-screen bg-slate-50">
