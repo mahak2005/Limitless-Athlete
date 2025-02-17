406bc4aea987b12829dab21b7bce9cd3ec81a1e4
Mahak <mahakyadav0520@gmail.com>
2025-02-18 02:35:10 +0530

    all done

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -3,41 +3,42 @@
 "use client";
 import { useAuth } from "@/components/auth/auth-context";
 import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
-// import { ProfileLayout } from "@/components/layout/profile-layout";
-// import { ProfileHeader } from "@/components/profile/profile-header";
+import { useEffect } from "react";
+// import { Footer } from "@/components/layout/footer";
+// import { ProfileHeader } from "@/components/profile/navbar";
+import { Navbar } from "@/components/layout/navbar";
 // import { ProfileContent } from "@/components/profile/profile-content";
 // import { ProfileSidebar } from "@/components/profile/profile-sidebar";
 // import { ProfileInfo } from "@/components/profile/profile-info";
+import { Footer } from "@/components/layout/footer";
 
 export default function ProfilePage() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { token, userId } = useAuth();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const router = useRouter();
 
-//   useEffect(() => {
-//     if (!token) {
-//       router.push("/login"); // Redirect to login if not authenticated
-//     }
-//   }, [token, router]);
+  useEffect(() => {
+    if (!token) {
+      router.push("/login"); // Redirect to login if not authenticated
+    }
+  }, [token, router]);
 
-//   if (!token) {
-//     return <p>Redirecting to login...</p>; // Prevents page flickering
-//   }
+  if (!token) {
+    return <p>Redirecting to login...</p>; // Prevents page flickering
+  }
 
-//   return (
+  return (
     
-//     <div className="min-h-screen">
-//       <Navbar />
+    <div className="min-h-screen">
+      <Navbar />
         
-//         <div className="flex flex-col xl:flex-row gap-6 flex-1">
-//           <ProfileContent />
+        <div className="flex flex-col xl:flex-row gap-6 flex-1">
+          {/* <ProfileContent /> */}
           
-//         </div>
+        </div>
      
-//       <Footer />
+      <Footer />
       
-//     </div>
-//   );
+    </div>
+  );
 }
