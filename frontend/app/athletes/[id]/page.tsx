a1b38b2be8120510dd0f4225689f7e7b80376cd0
Mahak <mahakyadav0520@gmail.com>
2025-02-18 00:17:01 +0530

    errorrrr

--- frontend/app/athletes/[id]/page.tsx
+++ frontend/app/athletes/[id]/page.tsx
@@ -1,29 +1,9 @@
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// import { Navbar } from "@/components/layout/navbar"
-// import { Footer } from "@/components/layout/footer"
-// import { AthleteProfile } from "@/components/sections/athlete-profile"
-
-// export default function AthletePage({ params }: { params: { id: string } }) {
-//   return (
-//     <div className="min-h-screen bg-slate-50">
-//       <Navbar />
-//       <main className="pt-16">
-//         <AthleteProfile id={params.id} />
-//       </main>
-//       <Footer />
-//     </div>
-//   )
-// }
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { AthleteProfile } from "@/components/sections/athlete-profile";
 
-interface AthletePageProps {
-  params: { id: string };
-}
-
-export default function AthletePage({ params }: AthletePageProps) {
+export default function AthletePage({ params }: { params: { id: string } }) {
   return (
     <div className="min-h-screen bg-slate-50">
       <Navbar />
